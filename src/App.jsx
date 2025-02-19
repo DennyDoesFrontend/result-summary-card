9aa344b5c79ebca7288dbeb41b37c71e98bf8312
Denzel Ashitey <denzelniiarmahashiteycode@gmail.com>
2025-02-19 18:32:18 +0000

    image src fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ import Container from './components/Container'
 function App() {
   const values = [
     {
-      imageUrl: "/src/assets/images/icon-reaction.svg",
+      imageUrl: "../src/assets/images/icon-reaction.svg",
       text: "Reaction",
       value: 80,
       color: "#ff575710",
       textColor: "#ff5757",
     },
     {
-      imageUrl: "/src/assets/images/icon-memory.svg",
+      imageUrl: "../src/assets/images/icon-memory.svg",
       text: "Memory",
       value: 92,
       color: "#ffb01f10",
       textColor: "#ffb01f"
     },
     {
-      imageUrl: "/src/assets/images/icon-verbal.svg",
+      imageUrl: "../src/assets/images/icon-verbal.svg",
       text: "Verbal",
       value: 61,
       color: "#00bd9110",
       textColor: "#00bd91"
     },
     {
-      imageUrl: "/src/assets/images/icon-visual.svg",
+      imageUrl: "../src/assets/images/icon-visual.svg",
       text: "Visual",
       value: 73,
       color: "#1125d410",
