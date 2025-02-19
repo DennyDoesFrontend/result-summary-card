f571c0a2f657053d8f12453ba26f21f28d6d7b5d
Denzel Ashitey <denzelniiarmahashiteycode@gmail.com>
2025-02-19 19:12:34 +0000

    left div alignment

--- src/App.jsx
+++ src/App.jsx
@@ -37,11 +37,11 @@ function App() {
   return (
 <div className='absolute bg-white top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] shadow-lg h-full md:h-auto sm:rounded-4xl overflow-hidden flex flex-col max-sm:w-full w-auto sm:flex-row'>
 {/* Left section */}
-      <div className='bg-gradient-to-b from-[#7857ff] to-[#2e2be9] p-10 flex flex-col gap-4 items-center rounded-b-4xl sm:rounded-4xl text-center w-full md:min-w-[320px] md:max-w-[350px] flex-[1_1_0]'>
+      <div className='bg-gradient-to-b from-[#7857ff] to-[#2e2be9] p-10 flex flex-col gap-4 items-center justify-between rounded-b-4xl sm:rounded-4xl text-center w-full md:min-w-[320px] md:max-w-[350px] flex-[1_1_0]'>
         <h2 className='text-2xl text-[#ffffff90] font-bold'>Your Result</h2>
         <div className='size-36 bg-gradient-to-b from-[#4e21ca] to-[#2421ca00] rounded-full flex flex-col items-center justify-center'>
-          <h3 className='text-7xl text-white font-bold leading-none'>76</h3>
-          <p className='text-[#ffffff90] font-semibold leading-none'>of 100</p>
+          <h3 className='text-7xl text-white font-bold'>76</h3>
+          <p className='text-[#ffffff90] font-semibold'>of 100</p>
         </div>
         <h3 className='text-3xl text-white font-bold'>Great</h3>
         <p className='text-[#ffffff90] text-semibold text-lg'>You scored higher than 65% of the people who have taken these tests.</p>
